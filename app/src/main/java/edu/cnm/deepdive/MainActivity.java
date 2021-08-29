0b72935b21cd0be6e3cae152f30c3dceb2e9c4e8
Marshall Eldridge <l2marshall@gmail.com>
2021-08-29 00:33:11 -0600

    All stats + and - buttons working.

--- app/src/main/java/edu/cnm/deepdive/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/MainActivity.java
@@ -8,50 +8,130 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
   }
 
+//Strength
   @SuppressLint("SetTextI18n")
   public void plusOneToStr(View view) {
-  TextView strengthValue = findViewById(R.id.strength_value);
-  float originalStrength = Float.parseFloat(strengthValue.getText().toString());
-  float updateStrength = originalStrength + 1F;
-  strengthValue.setText(Float.toString(updateStrength));
+    TextView value = findViewById(R.id.strength_value);
+    float original = Float.parseFloat(value.getText().toString());
+    float update = original + 1F;
+    value.setText(Float.toString(update));
   }
 
   @SuppressLint("SetTextI18n")
   public void minusOneToStr(View view) {
-    TextView strengthValue = findViewById(R.id.strength_value);
-    float originalStrength = Float.parseFloat(strengthValue.getText().toString());
-    if (originalStrength > 1) {
-      float updateStrength = originalStrength - 1F;
-      strengthValue.setText(Float.toString(updateStrength));
+    TextView value = findViewById(R.id.strength_value);
+    float original = Float.parseFloat(value.getText().toString());
+    if (original > 1) {
+      float update = original - 1F;
+      value.setText(Float.toString(update));
     } else {
-      strengthValue.setText(Float.toString(originalStrength));
+      value.setText(Float.toString(original));
     }
   }
-
+//Dexterity
   @SuppressLint("SetTextI18n")
   public void plusOneToDex(View view) {
-    TextView dexterityValue = findViewById(R.id.dexterity_value);
-    float originalDexterity = Float.parseFloat(dexterityValue.getText().toString());
-    float updateDexterity = originalDexterity + 1F;
-    dexterityValue.setText(Float.toString(updateDexterity));
+    TextView value = findViewById(R.id.dexterity_value);
+    float original = Float.parseFloat(value.getText().toString());
+    float update = original + 1F;
+    value.setText(Float.toString(update));
   }
 
   @SuppressLint("SetTextI18n")
   public void minusOneToDex(View view) {
-    TextView dexterityValue = findViewById(R.id.dexterity_value);
-    float originalDexterity = Float.parseFloat(dexterityValue.getText().toString());
-    if (originalDexterity > 1) {
-      float updateDexterity = originalDexterity - 1F;
-      dexterityValue.setText(Float.toString(updateDexterity));
+    TextView value = findViewById(R.id.dexterity_value);
+    float original = Float.parseFloat(value.getText().toString());
+    if (original > 1) {
+      float update = original - 1F;
+      value.setText(Float.toString(update));
     } else {
-      dexterityValue.setText(Float.toString(originalDexterity));
+      value.setText(Float.toString(original));
+    }
+  }
+//Intelligence
+  @SuppressLint("SetTextI18n")
+  public void plusOneToInt(View view) {
+    TextView value = findViewById(R.id.intelligence_value);
+    float original = Float.parseFloat(value.getText().toString());
+    float update = original + 1F;
+    value.setText(Float.toString(update));
+  }
+
+  @SuppressLint("SetTextI18n")
+  public void minusOneToInt(View view) {
+    TextView value = findViewById(R.id.intelligence_value);
+    float original = Float.parseFloat(value.getText().toString());
+    if (original > 1) {
+      float update = original - 1F;
+      value.setText(Float.toString(update));
+    } else {
+      value.setText(Float.toString(original));
+    }
+  }
+//Wisdom
+  @SuppressLint("SetTextI18n")
+  public void plusOneToWis(View view) {
+    TextView value = findViewById(R.id.wisdom_value);
+    float original = Float.parseFloat(value.getText().toString());
+    float update = original + 1F;
+    value.setText(Float.toString(update));
+  }
+
+  @SuppressLint("SetTextI18n")
+  public void minusOneToWis(View view) {
+    TextView value = findViewById(R.id.wisdom_value);
+    float original = Float.parseFloat(value.getText().toString());
+    if (original > 1) {
+      float update = original - 1F;
+      value.setText(Float.toString(update));
+    } else {
+      value.setText(Float.toString(original));
+    }
+  }
+//Agility
+  @SuppressLint("SetTextI18n")
+  public void plusOneToAgi(View view) {
+    TextView value = findViewById(R.id.agility_value);
+    float original = Float.parseFloat(value.getText().toString());
+    float update = original + 1F;
+    value.setText(Float.toString(update));
+  }
+
+  @SuppressLint("SetTextI18n")
+  public void minusOneToAgi(View view) {
+    TextView value = findViewById(R.id.agility_value);
+    float original = Float.parseFloat(value.getText().toString());
+    if (original > 1) {
+      float update = original - 1F;
+      value.setText(Float.toString(update));
+    } else {
+      value.setText(Float.toString(original));
+    }
+  }
+//Constitution
+  @SuppressLint("SetTextI18n")
+  public void plusOneToCon(View view) {
+    TextView value = findViewById(R.id.constitution_value);
+    float original = Float.parseFloat(value.getText().toString());
+    float update = original + 1F;
+    value.setText(Float.toString(update));
+  }
+
+  @SuppressLint("SetTextI18n")
+  public void minusOneToCon(View view) {
+    TextView value = findViewById(R.id.constitution_value);
+    float original = Float.parseFloat(value.getText().toString());
+    if (original > 1) {
+      float update = original - 1F;
+      value.setText(Float.toString(update));
+    } else {
+      value.setText(Float.toString(original));
     }
   }
 }
